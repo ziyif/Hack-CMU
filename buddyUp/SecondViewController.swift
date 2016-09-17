1851707e7ff29c745b0a18eed96e7ea105fe1c5a
Flora Feng <FloraFeng@floras-mbp.wv.cc.cmu.edu>
2016-09-16 21:33:35 -0400

    Initial Commit

---
+++ buddyUp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  buddyUp
+//
+//  Created by Flora Feng on 2016-09-16.
+//  Copyright © 2016 HackCMU. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
